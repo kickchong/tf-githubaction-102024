9c6757e1f29f8349e537c09822a7c72bdc3b0efb
kickchong <kickchong@gmail.com>
2024-10-15 21:42:37 -0700

    key

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bobby"
+    bucket         = "albert-terraform-state-bobby"
     region         = "us-east-1"
     key            = "s3-github-actions/terraform.tfstate"
     encrypt = true
